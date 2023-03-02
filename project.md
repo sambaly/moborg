cef906c13dee4fad2bed33ceafdbcd5d299468d8
kote.sambaly <kote.sambaly@gmail.com>
2023-03-01 21:42:39 -0500

    Added Dashboard endpoint.

--- project.md
+++ project.md
@@ -12,3 +12,17 @@ This project manages remote mob programming sessions:
 * Dashboard:
   + Create a new session
   + See sessions and how many registrations
+  
+1. See existing sessions
+   - Session name
+   - Session date/time (including timezone)
+   - Number of people registered
+   - Participant detail
+     - Name
+     - Github username
+     - Email address
+     - Discord username
+     - New to group ?
+Go to /dashboard, we'll see existing sessions
+
+2. Create new session
---
+++ src/test/java/com/africawalletsas/moborg/adapter/in/web/WebConfigurationTest.java
@@ -0,0 +1,22 @@
+package com.africawalletsas.moborg.adapter.in.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class WebConfigurationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getOfDashboardEndpointReturns200Ok() throws Exception {
+        mockMvc.perform(get("/dashboard"))
+                .andExpect(status().isOk());
+    }
+}
